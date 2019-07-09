049e838021c7a9957517cb2d633c7a9fa74cc71c
shunsuke sasaki <omu.omugin@gmail.com>
2019-07-09 23:03:24 +0900

    [Fix] correct name mapping for abi and rust target

--- rust/tools/build.sh
+++ rust/tools/build.sh
@@ -9,6 +9,9 @@ cargo build --target aarch64-linux-android --release
 cargo build --target armv7-linux-androideabi --release
 cargo build --target i686-linux-android --release
 
-cp ./target/aarch64-linux-android/release/${file_name} ${jni_path}/arm64/${file_name}
-cp ./target/armv7-linux-androideabi/release/${file_name} ${jni_path}/armeabi/${file_name}
+# directory name should be matched with ABI
+# see https://forge.rust-lang.org/platform-support.html for rust targets
+# see https://developer.android.com/ndk/guides/abis for abi name
+cp ./target/aarch64-linux-android/release/${file_name} ${jni_path}/arm64-v8a/${file_name}
+cp ./target/armv7-linux-androideabi/release/${file_name} ${jni_path}/armeabi-v7a/${file_name}
 cp ./target/i686-linux-android/release/${file_name} ${jni_path}/x86/${file_name}
