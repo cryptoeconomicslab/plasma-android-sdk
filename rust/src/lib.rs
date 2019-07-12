7e010c275600e7264471eff908040ac252375471
Shuhei Hiya <syuhei176@gmail.com>
2019-07-12 11:23:52 +0900

    Fix build with plasma-core crate #7

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,10 @@
 #![cfg(target_os = "android")]
 #![allow(non_snake_case)]
 
-// FIXME: add package below, for now some how failed to build
-// use ethereum_types::Address;
 use jni::objects::{JObject, JString};
 use jni::sys::jstring;
 use jni::JNIEnv;
+use plasma_core::data_structure::Range;
 use std::ffi::{CStr, CString};
 
 #[no_mangle]
@@ -18,8 +17,12 @@ pub unsafe extern "C" fn Java_com_cryptoeconomicslab_plasma_1android_1sdk_Extern
         env.get_string(j_recipient).unwrap().as_ptr(),
     ));
 
+    let range = Range::new(0, 100);
+
     let output = env
-        .new_string("Hello ".to_owned() + recipient.to_str().unwrap())
+        .new_string(
+            "Hello ".to_owned() + &format!("range={:?}", range) + recipient.to_str().unwrap(),
+        )
         .unwrap();
     output.into_inner()
 }
