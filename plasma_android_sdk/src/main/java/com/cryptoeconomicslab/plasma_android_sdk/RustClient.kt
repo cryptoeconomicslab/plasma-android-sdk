da53d8fd44c00939418d9316e4394946423e6795
shunsuke sasaki <omu.omugin@gmail.com>
2019-08-03 14:55:59 +0900

    [Add] javadoc comment for public methods

--- plasma_android_sdk/src/main/java/com/cryptoeconomicslab/plasma_android_sdk/RustClient.kt
+++ plasma_android_sdk/src/main/java/com/cryptoeconomicslab/plasma_android_sdk/RustClient.kt
@@ -5,11 +5,20 @@ import com.cryptoeconomicslab.plasma_android_sdk.hello_world.HelloWorld
 class RustClient {
 
     companion object {
+        /**
+         * This will load the Rust library.
+         * This method should be called once in your application.
+         */
         fun initialize() {
             System.loadLibrary("plasma_android_sdk")
         }
     }
 
+    /**
+     * Sample method for communicating with Rust layer.
+     * @param message String that will be passed to Rust layer.
+     * @return String which is modified in Rust layer.
+     */
     fun sayHello(message: String): String {
         return HelloWorld().hello(message)
     }
