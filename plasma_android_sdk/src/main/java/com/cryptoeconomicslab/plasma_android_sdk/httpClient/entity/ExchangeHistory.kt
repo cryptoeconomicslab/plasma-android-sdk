6eb27f7ccae41932fcf5f642dcf7e9debb3f648f
tkmct <1220t.takamichi@gmail.com>
2019-09-13 08:56:50 +0900

    Add exchangeId

--- plasma_android_sdk/src/main/java/com/cryptoeconomicslab/plasma_android_sdk/httpClient/entity/ExchangeHistory.kt
+++ plasma_android_sdk/src/main/java/com/cryptoeconomicslab/plasma_android_sdk/httpClient/entity/ExchangeHistory.kt
@@ -4,6 +4,7 @@ import java.time.LocalDateTime
 
 
 data class ExchangeHistory(
+    val exchangeId: String,
     val type: String, // 'OFFERED' | 'OFFERED'
     val tokenId: String,
     val amount: Int,
--- plasma_android_sdk/src/main/java/com/cryptoeconomicslab/plasma_android_sdk/httpClient/entity/ExchangeOffer.kt
+++ plasma_android_sdk/src/main/java/com/cryptoeconomicslab/plasma_android_sdk/httpClient/entity/ExchangeOffer.kt
@@ -2,6 +2,7 @@ package com.cryptoeconomicslab.plasma_android_sdk.httpClient.entity
 
 
 data class ExchangeOffer(
+    val exchangeId: String,
     val type: String,
     val tokenId: String,
     val amount: Int,
