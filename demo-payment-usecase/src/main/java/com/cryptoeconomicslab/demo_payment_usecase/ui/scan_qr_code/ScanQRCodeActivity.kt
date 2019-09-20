e26f8b38efd157a7eb12496266ad3093f9e09d46
omuomugin <omu.omugin@gmail.com>
2019-09-20 17:36:39 +0900

    feat: add qr code scan screen

--- demo-payment-usecase/src/main/java/com/cryptoeconomicslab/demo_payment_usecase/ui/scan_qr_code/ScanQRCodeActivity.kt
+++ demo-payment-usecase/src/main/java/com/cryptoeconomicslab/demo_payment_usecase/ui/scan_qr_code/ScanQRCodeActivity.kt
@@ -1,5 +1,6 @@
 package com.cryptoeconomicslab.demo_payment_usecase.ui.scan_qr_code
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
@@ -7,10 +8,12 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.cryptoeconomicslab.demo_payment_usecase.R
 
-class ScanQRCodeActivity : AppCompatActivity() {
+class ScanQRCodeActivity : AppCompatActivity(), Transition {
 
     companion object {
         fun createIntent(context: Context): Intent = Intent(context, ScanQRCodeActivity::class.java)
+
+        const val KEY_RESULT_QRCODE = "result_qrcode"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,14 +22,25 @@ class ScanQRCodeActivity : AppCompatActivity() {
 
         setTitle(R.string.screen__qr_name)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, ScanQRCodeFragment.getFragment(this))
+            .commit()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
+                setResult(RESULT_CANCELED, intent)
                 finish()
             }
         }
         return true
+    }
+
+    override fun finishWithResult(text: String) {
+        intent.putExtra(KEY_RESULT_QRCODE, text)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
