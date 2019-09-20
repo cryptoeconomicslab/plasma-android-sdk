29e586999d797c6ddafa2c128812ce63b153468c
omuomugin <omu.omugin@gmail.com>
2019-09-20 19:04:09 +0900

    feat: add exchange offer screen

--- demo-payment-usecase/src/main/java/com/cryptoeconomicslab/demo_payment_usecase/ui/home/exchange/ExchangeFragment.kt
+++ demo-payment-usecase/src/main/java/com/cryptoeconomicslab/demo_payment_usecase/ui/home/exchange/ExchangeFragment.kt
@@ -2,14 +2,20 @@ package com.cryptoeconomicslab.demo_payment_usecase.ui.home.exchange
 
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.cryptoeconomicslab.demo_payment_usecase.R
+import com.cryptoeconomicslab.demo_payment_usecase.repository.offer.OfferRepository
+import com.cryptoeconomicslab.demo_payment_usecase.repository.offer.OfferRepositoryMock
 import com.cryptoeconomicslab.demo_payment_usecase.ui.home.ActionBarCallback
 import com.cryptoeconomicslab.demo_payment_usecase.ui.home.Transition
+import com.cryptoeconomicslab.plasma_android_sdk.httpClient.entity.ExchangeOffer
 
 /**
  * A simple [Fragment] subclass.
@@ -33,12 +39,27 @@ class ExchangeFragment(
         actionBarCallback.setActionBarTitle(getString(R.string.screen__offer_name))
         actionBarCallback.clearActionBarMenu()
 
+        val repository: OfferRepository = OfferRepositoryMock()
+
         val view = inflater.inflate(R.layout.fragment_exchange, container, false)
 
         view.findViewById<Button>(R.id.add_button).apply {
             setOnClickListener {
                 transition.moveToNewOfferScreen()
             }
+        }
+
+        view.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            adapter = ExchangeViewAdapter(context, repository.getOffers()).apply {
+                setOnItemClickListener(object : ExchangeViewAdapter.ItemClickListener {
+                    override fun onItemClick(item: ExchangeOffer) {
+                        // TODO :
+                        Log.d("DEBUGGGG", item.toString())
+                    }
+                })
+            }
+
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
         }
 
         return view
