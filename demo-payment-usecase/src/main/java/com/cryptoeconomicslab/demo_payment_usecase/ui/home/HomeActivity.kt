4fee106f35df31ae1b0e630b860d62af3e35dcb5
tkmct <1220t.takamichi@gmail.com>
2019-10-03 19:16:21 +0900

    Fix address

--- demo-payment-usecase/src/main/java/com/cryptoeconomicslab/demo_payment_usecase/ui/home/HomeActivity.kt
+++ demo-payment-usecase/src/main/java/com/cryptoeconomicslab/demo_payment_usecase/ui/home/HomeActivity.kt
@@ -1,13 +1,18 @@
 package com.cryptoeconomicslab.demo_payment_usecase.ui.home
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.cryptoeconomicslab.demo_payment_usecase.R
+import com.cryptoeconomicslab.demo_payment_usecase.repository.wallet.WalletRepositoryImpl
 import com.cryptoeconomicslab.demo_payment_usecase.ui.home.exchange.ExchangeFragment
 import com.cryptoeconomicslab.demo_payment_usecase.ui.home.payment.PaymentFragment
 import com.cryptoeconomicslab.demo_payment_usecase.ui.home.wallet.WalletFragment
@@ -26,10 +31,26 @@ class HomeActivity : AppCompatActivity(), Transition, ActionBarCallback {
     }
 
     lateinit var bottomNavigationView: BottomNavigationView
+    lateinit var addressText: TextView
+    lateinit var copyAddressButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val repository = WalletRepositoryImpl(this)
+        addressText = findViewById<TextView>(R.id.address_text).apply {
+            this.text = repository.getAddress()
+        }
+
+        copyAddressButton = findViewById<Button>(R.id.copy_clipboard_button).apply {
+            this.setOnClickListener {
+                val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = ClipData.newPlainText("wallet address", repository.getAddress())
+                clipboard.setPrimaryClip(clip)
+
+            }
+        }
 
         bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation).apply {
             setOnNavigationItemSelectedListener { item ->
